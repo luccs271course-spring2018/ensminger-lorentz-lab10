1cd8fa4714d1a09c7a9e5e104f036ad1ece47fe8
Taylor MillerEnsminger <tmillerensminger@luc.edu>
2017-12-05 15:07:09 +0000

    edges + vertex

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -12,5 +12,24 @@ public class Main {
    final static String HR = "Croatia";
    final static String SI = "Slovenia";
    final static String AT = "Austria";
+   final Graph<String, DefaultEdge> myMap = new SimpleGraph<>(DefaultEdge.class);
+   myMap.addVertex(HU);
+   myMap.addVertex(SK);
+   myMap.addVertex(RO);
+   myMap.addVertex(RS);
+   myMap.addVertex(HR);
+   myMap.addVertex(SI);
+   myMap.addVertex(AT);
+   myMap.addEdge(HU, SK);
+   myMap.addEdge(HU, RO);
+   myMap.addEdge(HU, RS);
+   myMap.addEdge(HU, HR);
+   myMap.addEdge(HU, SI);
+   myMap.addEdge(HU, AT);
+   myMap.addEdge(RO, RS);
+   myMap.addEdge(RS, HR);
+   myMap.addEdge(HR, SI);
+   myMap.addEdge(SI, AT);
+   myMap.addEdge(AT, SK);
   }
 }
