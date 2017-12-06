7eebd52237a4d0e33222a7a2d3c180aec29bdc95
Taylor MillerEnsminger <tmillerensminger@luc.edu>
2017-12-06 14:47:57 +0000

    missing coloring

--- src/main/java/hw/Main.java
+++ src/main/java/hw/Main.java
@@ -5,72 +5,68 @@ import java.util.Arrays;
 public class Main {
 
   public static void main(final String[] args) {
-   final static String HU = "Hungary";
-   final static String SK = "Slovakia";
-   final static String RO = "Romania";
-   final static String RS = "Serbia";
-   final static String HR = "Croatia";
-   final static String SI = "Slovenia";
-   final static String AT = "Austria";
+   Countries countries = new Countries();
    final Graph<String, DefaultEdge> myMap = new SimpleGraph<>(DefaultEdge.class);
-   myMap.addVertex(HU);
-   myMap.addVertex(SK);
-   myMap.addVertex(RO);
-   myMap.addVertex(RS);
-   myMap.addVertex(HR);
-   myMap.addVertex(SI);
-   myMap.addVertex(AT);
-   myMap.addEdge(HU, SK);
-   myMap.addEdge(HU, RO);
-   myMap.addEdge(HU, RS);
-   myMap.addEdge(HU, HR);
-   myMap.addEdge(HU, SI);
-   myMap.addEdge(HU, AT);
-   myMap.addEdge(RO, RS);
-   myMap.addEdge(RS, HR);
-   myMap.addEdge(HR, SI);
-   myMap.addEdge(SI, AT);
-   myMap.addEdge(AT, SK);
+   myMap.addVertex(countries.getHU());
+   myMap.addVertex(countries.getSK());
+   myMap.addVertex(countries.getRO());
+   myMap.addVertex(countries.getRS());
+   myMap.addVertex(countries.getHR());
+   myMap.addVertex(countries.getSI());
+   myMap.addVertex(countries.getAT());
+   myMap.addEdge(countries.getHU(), countries.getSK());
+   myMap.addEdge(countries.getHU(), countries.getRO());
+   myMap.addEdge(countries.getHU(), countries.getRS());
+   myMap.addEdge(countries.getHU(), countries.getHR());
+   myMap.addEdge(countries.getHU(), countries.getSI());
+   myMap.addEdge(countries.getHU(), countries.getAT());
+   myMap.addEdge(countries.getRO(), countries.getRS());
+   myMap.addEdge(countries.getRS(), countries.getHR());
+   myMap.addEdge(countries.getHR(), countries.getSI());
+   myMap.addEdge(countries.getSI(), countries.getAT());
+   myMap.addEdge(countries.getAT(), countries.getSK());
    System.out.println(myMap);
-   final Iterator<String> bf = new BreadthFirstIterator<>(myMap, HU);
+   final Iterator<String> bf = new BreadthFirstIterator<>(myMap, countries.getHU());
    while (bf.hasNext()) {
       final String country = bf.next();
       System.out.println(country);
    }
-   final Iterator<String> bt = new BreadthFirstIterator<>(myMap, SK);
+   final Iterator<String> bt = new BreadthFirstIterator<>(myMap, countries.getSK());
    while (bf.hasNext()) {
     final String country = bf.next();
     System.out.println(country);
  }
-  final Iterator<String> cf = new ClosestFirstIterator<>(myMap, HU);
+  final Iterator<String> cf = new ClosestFirstIterator<>(myMap, countries.getHU());
  while (cf.hasNext()) {
    final String country = cf.next();
     System.out.println(country);
  }
-   final Iterator<String> ct = new ClosestFirstIterator<>(myMap, SK);
+   final Iterator<String> ct = new ClosestFirstIterator<>(myMap, countries.getSK());
  while (bf.hasNext()) {
    final String country = bf.next();
    System.out.println(country);
  }
- final Iterator<String> df = new DepthFirstIterator<>(myMap, HU);
+ final Iterator<String> df = new DepthFirstIterator<>(myMap, countries.getHU());
  while (cf.hasNext()) {
    final String country = cf.next();
     System.out.println(country);
  }
-   final Iterator<String> dt = new DepthFirstIterator<>(myMap, SK);
+   final Iterator<String> dt = new DepthFirstIterator<>(myMap, countries.getSK());
  while (bf.hasNext()) {
    final String country = bf.next();
    System.out.println(country);
  }
- final Iterator<String> rf = new RandomWalkIterator<>(myMap, HU);
+ final Iterator<String> rf = new RandomWalkIterator<>(myMap, countries.getHU());
  while (cf.hasNext()) {
    final String country = cf.next();
     System.out.println(country);
  }
-   final Iterator<String> rt = new RandomWalkIterator<>(myMap, SK);
+   final Iterator<String> rt = new RandomWalkIterator<>(myMap, countries.getSK());
  while (bf.hasNext()) {
    final String country = bf.next();
    System.out.println(country);
  }
   }
+  int cnum = findGreedyChromaticNumber(myMap);
+  System.out.println("The min Number of colors is: " + cnum);
 }
